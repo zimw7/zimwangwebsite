b4c23fb38789061ab60da0af42f666e6ff357d09
Zimeng Wang <134750873+zimw7@users.noreply.github.com>
2023-10-31 16:06:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* 
+Name: Zimeng Wang
+File Name: script.js
+Create Date: Oct 29, 2023 
+Purpose: This JavaScript file contains functions to control the navigation menu and image carousel on the web page.
+*/
+
+// Function to toggle the mobile navigation menu
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -5,20 +13,22 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-
-
+// Variables for controlling the image carousel
 var items = document.querySelectorAll('.carousel-item');
 var totalItems = items.length;
 var currentIndex = 0;
 
+// Initialize the first item as active
 items[currentIndex].classList.add('active');
 
+// Event listener for the "Next" button
 document.querySelector('.next').addEventListener('click', function() {
   items[currentIndex].classList.remove('active');
   currentIndex = (currentIndex + 1) % totalItems;
   items[currentIndex].classList.add('active');
 });
 
+// Event listener for the "Previous" button
 document.querySelector('.prev').addEventListener('click', function() {
   items[currentIndex].classList.remove('active');
   currentIndex = (currentIndex - 1 + totalItems) % totalItems;
